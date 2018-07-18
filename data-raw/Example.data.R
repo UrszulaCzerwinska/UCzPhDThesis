5eda7298f36651a7ced9ce9010dba06a95f940b3
urszulaczerwinska <urszula.czerwinska@cri-paris.org>
2018-07-18 17:42:47 +0200

    discussion, conclusions & perspectives

--- data-raw/Example.data.R
+++
@@ -1,9 +0,0 @@
-setwd("./data-raw/")
-BEK_log_cen <-
-  read.csv("BRCABEK.txt",
-           sep = "",
-           stringsAsFactors = FALSE)
-Example_ds <- BEK_log_cen[,c(1, sample(2:ncol(BEK_log_cen),60))]
-devtools::use_data(Example_ds, overwrite = TRUE)
-
-
--- data-raw/import_LM22.R
+++
@@ -1,3 +0,0 @@
-LM22 <- read.table("./inst/data-raw/LM22.txt", sep= "\t", header= TRUE, row.names = 1)
-LM22.list <- make_list(LM22)
-devtools::use_data(LM22.list)
